9980b2e8f6c1f1e5065060f975f7f63dba3973fc
Sergei Zhekpisov <7286747+vertrost@users.noreply.github.com>
2021-07-04 22:27:01 +0300

    loop fix

--- pulumi/2_conditions/__main__.py
+++ pulumi/2_conditions/__main__.py
@@ -8,11 +8,11 @@ config = pulumi.Config();
 color = config.require('color');
 for i in range(3):
     if color == 'red':
-        bucket = s3.Bucket('my-bucket-rose')
+        bucket = s3.Bucket('my-bucket-rose' + '-' + str(i))
     elif color =='blue':
-        bucket = s3.Bucket('my-bucket-tulip')
+        bucket = s3.Bucket('my-bucket-tulip' + '-' + str(i))
     else:
-        bucket = s3.Bucket('my-bucket-no-flower')
+        bucket = s3.Bucket('my-bucket-no-flower' + '-' + str(i))
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
