d29e1c82f7fbae34d535dbe74c16f5f834fb0d13
Sergei Zhekpisov <7286747+vertrost@users.noreply.github.com>
2021-07-04 22:26:21 +0300

    add pulumi and awscf

--- pulumi/2_conditions/__main__.py
+++ pulumi/2_conditions/__main__.py
@@ -6,12 +6,13 @@ from pulumi_aws import s3
 # Create an AWS resource (S3 Bucket)
 config = pulumi.Config();
 color = config.require('color');
-if color == 'red':
-    bucket = s3.Bucket('my-bucket-rose')
-elif color =='blue':
-    bucket = s3.Bucket('my-bucket-tulip')
-else:
-    bucket = s3.Bucket('my-bucket-no-flower')
+for i in range(3):
+    if color == 'red':
+        bucket = s3.Bucket('my-bucket-rose')
+    elif color =='blue':
+        bucket = s3.Bucket('my-bucket-tulip')
+    else:
+        bucket = s3.Bucket('my-bucket-no-flower')
 
 # Export the name of the bucket
 pulumi.export('bucket_name', bucket.id)
